6afeeb4fe4426ead717aa9fc3dba4f2662d36ddc
sooraj <sooraj.a.i@gmail.com>
2025-08-16 11:56:36 +0000

    feat: add volatility and high low diff plots

--- src/data_plotter.py
+++ src/data_plotter.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import seaborn as sns
+import numpy as np
 import pandas as pd
 from plotly.subplots import make_subplots
 import logging
@@ -8,76 +9,145 @@ class PlotterClass:
     "Class for plotting financial data"
     template = 'plotly_dark'
     
-    @staticmethod
-    def plot_closing_data(market_data: pd.DataFrame):
-        "Plot closing data from the market data "
-        closing_fig = go.Figure()
-        closing_fig.add_trace(go.Scatter(
-            x=market_data["Date"].
-            y=market_data["Close"],
-            mode='lines',
-            name='Close Price'
-        ))
-        closing_fig.update_layout(
-            title='Closing Price Over Time',
-            xaxis_title='Date',
-            yaxis_title='Price',
-            template=PlotterClass.template
-        )
-        closing_fig.show()
-    @staticmethod
-    def plot_candlestick_data(market_data: pd.DataFrame, sma1: float = 0, sma2: float = 0):
-        "Plot candlestick data from the market data"
-        if market_data.empty:
-            logging.warning("Market data is empty. Cannot plot candlestick chart.")
-            return  
-        candlestick_fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02, row_heights=[0.7, 0.3])
-        candlestick_fig.add_trace(go.Candlestick(
-            x=market_data["Date"],
-            open=market_data["Open"],
-            high=market_data["High"],
-            low=market_data["Low"],
-            close=market_data["Close"],
-            name='Candlestick'
-        ), row=1, col=1)
-        candlestick_fig.update_traces(increasing_line_width=1.5, decreasing_line_width=1.5)
-        if sma1 > 0:
-            candlestick_fig.add_trace(go.Scatter(
+    class MarketData:
+        @staticmethod
+        def plot_closing_data(market_data: pd.DataFrame):
+            "Plot closing data from the market data "
+            closing_fig = go.Figure()
+            closing_fig.add_trace(go.Scatter(
                 x=market_data["Date"],
-                y=market_data["Close"].rolling(window=int(sma1)).mean(),
+                y=market_data["Close"],
                 mode='lines',
-                name=f'SMA {sma1} - moving day average',
-                line=dict(color='lightblue', width=1.5)
-            ), row=1, col=1)
-        if sma2 > 0:
-            candlestick_fig.add_trace(go.Scatter(
-                x=market_data["Date"],
-                y=market_data["Close"].rolling(window=int(sma2)).mean(),
-                mode='lines',
-                name=f'SMA {sma2} - moving day average',
-                line=dict(color='red', width=1.5)
-            ), row=1, col=1)
-        candlestick_fig.add_trace(go.Bar(
-            x=market_data["Date"],
-            y=market_data["Volume"],
-            name='Volume',
-        ), row=2, col=1)
-        candlestick_fig.update_layout(
-            title = {
-                'text': f' {market_data["Symbol"]} Candlestick Chart with volume',
-                'x' : 0.5,
-                'y': 0.9,
-                'font': {'size' : 24},
-                'xanchor': 'center',
-                'yanchor': 'top',},
-                xaxis_rangeslider_visible=False,
+                name='Close Price'
+            ))
+            closing_fig.update_layout(
+                title='Closing Price Over Time',
                 xaxis_title='Date',
-                yaxis_title='Price (USD)',
-                hovermode='x',
-                bargap = 0,
-                bargroupgap = 0.0,
-                template=PlotterClass.template)
-        candlestick_fig.update_xaxes(title_text = '',row=1, col=1, showgrid=True)
-        candlestick_fig.update_xaxes(title_text = 'Date',row=1, col=1)
-        candlestick_fig.update_yaxes(title_text = 'Volume',row=1, col=1)
-        candlestick_fig.show()
+                yaxis_title='Price',
+                template=PlotterClass.template
+            )
+            return closing_fig
+        
+        @staticmethod
+        def plot_candlestick_data(market_data: pd.DataFrame, sma1: int = 0, sma2: int = 0):
+            "Plot candlestick data from the market data"
+            if market_data.empty:
+                logging.warning("Market data is empty. Cannot plot candlestick chart.")
+                return  
+            candlestick_fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02, row_heights=[0.7, 0.3])
+            candlestick_fig.add_trace(go.Candlestick(
+                x=market_data["Date"],
+                open=market_data["Open"],
+                high=market_data["High"],
+                low=market_data["Low"],
+                close=market_data["Close"],
+                name='Candlestick'
+            ), row=1, col=1)
+            candlestick_fig.update_traces(increasing_line_width=1.5, decreasing_line_width=1.5)
+            if sma1 > 0:
+                candlestick_fig.add_trace(go.Scatter(
+                    x=market_data["Date"],
+                    y=market_data["Close"].rolling(window=int(sma1)).mean(),
+                    mode='lines',
+                    name=f'SMA {sma1} - moving day average',
+                    line=dict(color='lightblue', width=1.5)
+                ), row=1, col=1)
+            if sma2 > 0:
+                candlestick_fig.add_trace(go.Scatter(
+                    x=market_data["Date"],
+                    y=market_data["Close"].rolling(window=int(sma2)).mean(),
+                    mode='lines',
+                    name=f'SMA {sma2} - moving day average',
+                    line=dict(color='red', width=1.5)
+                ), row=1, col=1)
+            candlestick_fig.add_trace(go.Bar(
+                x=market_data["Date"],
+                y=market_data["Volume"],
+                name='Volume',
+            ), row=2, col=1)
+            candlestick_fig.update_layout(
+                title = {
+                    'text': f' {market_data["Symbol"]} Candlestick Chart with volume',
+                    'x' : 0.5,
+                    'y': 0.9,
+                    'font': {'size' : 24},
+                    'xanchor': 'center',
+                    'yanchor': 'top',},
+                    xaxis_rangeslider_visible=False,
+                    xaxis_title='Date',
+                    yaxis_title='Price (USD)',
+                    hovermode='x',
+                    bargap = 0,
+                    bargroupgap = 0.0,
+                    template=PlotterClass.template)
+            candlestick_fig.update_xaxes(title_text = '',row=1, col=1, showgrid=True)
+            candlestick_fig.update_xaxes(title_text = 'Date',row=2, col=1)
+            candlestick_fig.update_yaxes(title_text = 'Volume',row=2, col=1)
+            return candlestick_fig
+        
+        @staticmethod
+        def plot_rolling_volatility(market_data: pd.DataFrame, window:int = 30):
+            "Plot rolling volatility from the market data"
+            if market_data.empty:
+                logging.warning("Market data is empty. Cannot plot rolling volatility.")
+                return
+            local_market_data = market_data.copy()
+            local_market_data["returns"] = local_market_data["Close"].pct_change()
+            local_market_data["volatility"] = local_market_data["returns"].rolling(window=window).std()
+            local_market_data["log_returns"] = np.log(local_market_data["Close"] / local_market_data["Close"].shift(1))
+            local_market_data["volatility_log"] = local_market_data["log_returns"].rolling(window=window).sstd()
+            volatility_fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02, row_heights=[0.7, 0.3])
+            volatility_fig.add_trace(go.Scatter(
+                x=local_market_data["Date"],
+                y=local_market_data["Volatility"],
+                mode='lines',
+                name='Rolling Volatility'
+            ), row=1, col=1)
+            volatility_fig.add_trace(go.Scatter(
+                x=local_market_data["Date"],
+                y=local_market_data["Volatility_log"],
+                mode='lines',
+                name='Log Returns Volatility'
+            ), row=1, col=1)
+            volatility_fig.add_trace(go.Scatter(
+                x=local_market_data["Date"],
+                y=local_market_data["Returns"],
+                mode='lines',
+                name='Returns'
+            ), row=2, col=1)
+            volatility_fig.add_trace(go.Scatter(
+                x=local_market_data["Date"],
+                y=local_market_data["Log_returns"],
+                mode='lines',
+                name='Log Returns'
+            ), row=2, col=1)
+            volatility_fig.update_layout(
+                title="Returns and Rolling Volatility Over Time",
+                template=PlotterClass.template
+            )
+            return volatility_fig
+            
+        @staticmethod
+        def plot_high_low_range(market_data: pd.DataFrame):
+            if market_data.empty:
+                logging.warning("Market data is empty. Cannot plot high-low range.")
+                return
+            high_low_fig = go.Figure()
+            high_low_fig.add_trace(go.Scatter(
+                x=market_data["Date"],
+                y=market_data["High"] - market_data["Low"],
+                mode='lines',
+                name='High Price',
+                line=dict(color='green')
+            ))
+            high_low_fig.update_layout(
+                title='High-Low Range Over Time',
+                xaxis_title='Date',
+                yaxis_title='Price Range (USD)',
+                template=PlotterClass.template
+            )
+            return high_low_fig
+            
+
+
+                
